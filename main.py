f4c1a7b37ee16356f9dc3152d3fb9b6446a592d5
Maximiliano Fernandez <45922974+Maxi91f@users.noreply.github.com>
2025-06-26 16:45:10 -0300

    funcionando

--- main.py
+++ main.py
@@ -1,11 +1,14 @@
 from fastmcp import FastMCP
 
-mcp = FastMCP("Demo para Python Paraguay")
+mcp = FastMCP("Demo para Python Paraguay", )
 
 @mcp.tool
-def add(a: int, b: int) -> int:
+def sumador(a: int, b: int) -> int:
     """Suma dos números"""
     return a + b
 
 if __name__ == "__main__":
-    mcp.run()
+    mcp.run(
+        transport="sse",
+        path="/mcp",
+    )
